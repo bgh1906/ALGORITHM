adc85e15e085316449fb4466d5c8591f5d214773
BaeJiHwan <bgh1906@naver.com>
2022-07-05 23:57:38 +0900

    백준_220705_11654_아스키코드

---
+++ 백준_220705_11654_아스키코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 백준_220705_11654_아스키코드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int answer = sc.next().charAt(0);
+
+        System.out.println(answer);
+    }
+}
