553c1fd9acd222369b8a509c3a95bf09c8fa4bf9
BaeJiHwan <bgh1906@naver.com>
2022-07-17 23:01:54 +0900

    백준_220717_10870_피보나치수5

---
+++ 백준_220717_10870_피보나치수5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class 백준_220717_10870_피보나치수5 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[21];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        if (n == 0)
+            System.out.println(0);
+        else if (n == 1)
+            System.out.println(1);
+        else {
+            for (int i = 0; i < n - 1; i++)
+                arr[i + 2] = arr[i] + arr[i + 1];
+            System.out.println(arr[n]);
+        }
+    }
+}
