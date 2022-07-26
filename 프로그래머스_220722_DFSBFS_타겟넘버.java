a2673dee366163bdccfcd1fb307964b4e8c19d00
BaeJiHwan <bgh1906@naver.com>
2022-07-26 21:59:42 +0900

    주석처리

--- 프로그래머스_220722_DFSBFS_타겟넘버.java
+++ 프로그래머스_220722_DFSBFS_타겟넘버.java
@@ -1,19 +1,19 @@
-class Solution {
-    private static int answer = 0;
-    public int solution(int[] numbers, int target) {
-        findTargetNumber(numbers, target, 0, 0);
-
-        return answer;
-    }
-
-    public static void findTargetNumber(int[] numbers, int target, int node, int result) {
-        if (node == numbers.length) {
-            if (result == target)
-                answer++;
-            return;
-        }
-
-        findTargetNumber(numbers, target, node + 1, result + numbers[node]);
-        findTargetNumber(numbers, target, node + 1, result - numbers[node]);
-    }
-}
+//class Solution {
+//    private static int answer = 0;
+//    public int solution(int[] numbers, int target) {
+//        findTargetNumber(numbers, target, 0, 0);
+//
+//        return answer;
+//    }
+//
+//    public static void findTargetNumber(int[] numbers, int target, int node, int result) {
+//        if (node == numbers.length) {
+//            if (result == target)
+//                answer++;
+//            return;
+//        }
+//
+//        findTargetNumber(numbers, target, node + 1, result + numbers[node]);
+//        findTargetNumber(numbers, target, node + 1, result - numbers[node]);
+//    }
+//}
