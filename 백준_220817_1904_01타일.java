ff5ba25dcd732f91b7c94e998d4f06f73c5a9a5b
BaeJiHwan <bgh1906@naver.com>
2022-08-18 22:40:16 +0900

    백준_220817_1904_01타일

--- 백준_220817_1904_01타일.java
+++ 백준_220817_1904_01타일.java
@@ -1,30 +1,26 @@
 import java.io.*;
  
 public class 백준_220817_1904_01타일 {
- 
 	public static int[] dp = new int[1000001];;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int N = Integer.parseInt(br.readLine());
 
 		dp[0] = 0;
 		dp[1] = 1;
 		dp[2] = 2;
- 
+
 		for(int i = 3; i < dp.length; i++) {
 			dp[i] = -1;
 		}
-		
 		System.out.println(MakeTile(N));
 	}
-	
+
 	public static int MakeTile(int N) {
 		if(dp[N] == -1) {
 			dp[N] = (MakeTile(N - 1) + MakeTile((N - 2))) % 15746;
 		}
 		return dp[N];
 	}
- 
 }
