78c345754d1dee115a6d2aa263cec28192a46026
BaeJiHwan <bgh1906@naver.com>
2022-08-27 23:50:35 +0900

    프로그래머스_220827_해시_완주하지못한선수

---
+++ 프로그래머스_220827_해시_완주하지못한선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class 프로그래머스_220827_해시_완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String player : participant) 
+            map.put(player, map.getOrDefault(player, 0) + 1);
+
+        for (String player : completion) 
+            map.put(player, map.get(player) - 1);
+
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        return answer;
+    }
+}
