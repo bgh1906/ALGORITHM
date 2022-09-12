c6304d251dce5b46131eaf7919456617c813c5c7
BaeJiHwan <bgh1906@naver.com>
2022-09-12 20:42:08 +0900

    백준_220912_10926_물음표물음표느낌표

---
+++ 백준_220912_10926_물음표물음표느낌표.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class 백준_220912_10926_물음표물음표느낌표 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "??!");
+    }
+}
