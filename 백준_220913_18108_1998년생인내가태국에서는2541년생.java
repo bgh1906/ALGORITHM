d45d15de6babb201cc7bc18ba460b5208fb1fdb1
BaeJiHwan <bgh1906@naver.com>
2022-09-13 23:37:46 +0900

    백준_220913_18108_1998년생인내가태국에서는2541년생

---
+++ 백준_220913_18108_1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class 백준_220913_18108_1998년생인내가태국에서는2541년생 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) - 543);
+    }
+}
